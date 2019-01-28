2772a64ab6fd1019c2edc966aabba42d85eb8032
em2046 <em2046@qq.com>
2019-01-28 18:16:11 +0800

    button

---
+++ src/components/Button.tsx
@@ -0,0 +1,31 @@
+import React, { useState } from 'react';
+
+interface Props {
+  type?: string;
+}
+
+function Button(props: Props) {
+  const type = props.type;
+  const [loading, setLoading] = useState(false);
+
+  function handleClick() {
+    setLoading(!loading);
+  }
+
+  let classNameList = [];
+  classNameList.push('cobalt-button');
+
+  if (type !== '') {
+    classNameList.push('cobalt-button-' + type);
+  }
+
+  return (
+    <div>
+      <button className={classNameList.join(' ')} onClick={handleClick}>
+        {loading ? 'loading' : ''} Click me
+      </button>
+    </div>
+  );
+}
+
+export default Button;
