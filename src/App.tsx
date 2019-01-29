8db1084746bbd73682f7776d2119c8c0d152b06d
em2046 <em2046@qq.com>
2019-01-29 14:23:07 +0800

    button

--- src/App.tsx
+++ src/App.tsx
@@ -3,27 +3,31 @@ import './App.css';
 
 import Button from './components/button/Button';
 
+function handleClick(e: any) {
+  console.log(e);
+}
+
 class App extends Component {
   render() {
     return (
       <div className="App">
         <section>
-          <Button />
-          <Button type="primary" />
-          <Button type="warning" />
-          <Button type="danger" />
+          <Button onClick={handleClick} />
+          <Button type="primary" onClick={handleClick} />
+          <Button type="warning" onClick={handleClick} />
+          <Button type="danger" onClick={handleClick} />
         </section>
         <section>
-          <Button loading={true} />
-          <Button type="primary" loading={true} />
-          <Button type="warning" loading={true} />
-          <Button type="danger" loading={true} />
+          <Button loading={true} onClick={handleClick} />
+          <Button type="primary" loading={true} onClick={handleClick} />
+          <Button type="warning" loading={true} onClick={handleClick} />
+          <Button type="danger" loading={true} onClick={handleClick} />
         </section>
         <section>
-          <Button disabled={true} />
-          <Button type="primary" disabled={true} />
-          <Button type="warning" disabled={true} />
-          <Button type="danger" disabled={true} />
+          <Button disabled onClick={handleClick} />
+          <Button type="primary" disabled onClick={handleClick} />
+          <Button type="warning" disabled onClick={handleClick} />
+          <Button type="danger" disabled onClick={handleClick} />
         </section>
       </div>
     );
--- src/components/button/Button.tsx
+++ src/components/button/Button.tsx
@@ -1,30 +1,35 @@
 import './button.less';
 import ClassNameBuild from '../../utils/class-name-builder';
-import React, { useState } from 'react';
+import * as React from 'react';
 
 type buttonType = 'default' | 'primary' | 'warning' | 'danger';
 
-interface Props {
+export interface Props {
   type?: buttonType;
   loading?: boolean;
   disabled?: boolean;
   size?: string;
   children?: any;
+  onClick?: any;
 }
 
-function Button(props: Props) {
+export default function Button(props: Props) {
   const prefixClass = 'cobalt-button';
-  const { type, loading, disabled } = props;
+  const { type, loading, onClick, ...rest } = props;
 
   let classNameBuild = new ClassNameBuild(prefixClass, {
     [`${prefixClass}-default`]: !type,
     [`${prefixClass}-${type}`]: type,
-    [`${prefixClass}-loading`]: loading,
-    [`${prefixClass}-disabled`]: disabled
+    [`${prefixClass}-loading`]: loading
   });
 
-  function handleClick() {
-    console.log('click');
+  function handleClick(e: any) {
+    if (loading) {
+      return;
+    }
+    if (onClick) {
+      onClick(e);
+    }
   }
 
   let children = props.children ? props.children : 'Button';
@@ -32,11 +37,9 @@ function Button(props: Props) {
   let className = classNameBuild.toString();
 
   return (
-    <button className={className} onClick={handleClick}>
+    <button className={className} onClick={handleClick} {...rest}>
       {children}
       {loading ? '...' : ''}
     </button>
   );
 }
-
-export default Button;
