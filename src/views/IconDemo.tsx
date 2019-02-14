65750851fa38ea9214a76475d83bd08da99c5782
em2046 <em2046@qq.com>
2019-02-14 13:40:08 +0800

    icon

--- src/views/IconDemo.tsx
+++ src/views/IconDemo.tsx
@@ -1,17 +1,16 @@
 import React from 'react';
 import Icon from '../components/icon';
-import { iconList, IconType } from '../components/icon/icon-sheet-generated';
+import { iconList } from '../components/icon/icon-sheet-generated';
 
 export default function ModalDemo() {
-  let strings = iconList as IconType[];
   return (
     <div className="Demo">
       <h1>Icon</h1>
       <h2>Example</h2>
-      <h3>Basic</h3>
+      <h3>Suggested Icon</h3>
       <div className="Demo-icon-list">
         <ul>
-          {strings.map(item => {
+          {iconList.map(item => {
             return (
               <li key={item}>
                 <Icon type={item} />
