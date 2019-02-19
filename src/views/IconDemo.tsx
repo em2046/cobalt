1df0800c38969ac6324cdd6db466200322c0b0a2
em2046 <em2046@qq.com>
2019-02-19 22:38:56 +0800

    icon group

--- src/views/IconDemo.tsx
+++ src/views/IconDemo.tsx
@@ -1,25 +1,31 @@
 import React from 'react';
 import { Icon } from 'cobalt';
-import { iconList } from '@components/icon/icon-sheet-generated';
+import { iconList } from '@components/icon/icon-list-generated';
 
 export default function ModalDemo() {
   return (
     <div className="Demo">
       <h1>Icon</h1>
       <h2>Example</h2>
-      <h3>Suggested Icon</h3>
-      <div className="Demo-icon-list">
-        <ul>
-          {iconList.map(item => {
-            return (
-              <li key={item}>
-                <Icon type={item} />
-                <span>{item}</span>
-              </li>
-            );
-          })}
-        </ul>
-      </div>
+      {iconList.map(group => {
+        return (
+          <React.Fragment key={group.group}>
+            <h3>{group.group} Icon</h3>
+            <div className="Demo-icon-list">
+              <ul>
+                {group.children.map(item => {
+                  return (
+                    <li key={item}>
+                      <Icon type={item} />
+                      <span>{item}</span>
+                    </li>
+                  );
+                })}
+              </ul>
+            </div>
+          </React.Fragment>
+        );
+      })}
     </div>
   );
 }
